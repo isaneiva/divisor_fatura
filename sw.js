6de6b6d504df41b0159a86d87e9b0c7556083279
Isabela Neiva <91497937+isaneiva@users.noreply.github.com>
2026-08-04 16:18:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-const CACHE = "divisor-v1";
+const CACHE = "divisor-v2";
 const ARQ = ["./", "./index.html", "./manifest.webmanifest",
-             "./icons/icon-192.png", "./icons/icon-512.png", "./icons/icon-512-maskable.png"];
+             "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQ)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => Promise.all(ARQ.map(u => c.add(u).catch(() => {}))))
+      .then(() => self.skipWaiting())
+  );
 });
 self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", e => {
   if (e.request.method !== "GET") return;
